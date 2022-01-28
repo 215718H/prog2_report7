e9fca2915382d62a08aa3811552f82ee341edb24
Ikki Matsuo <e215718@ie.u-ryukyu.ac.jp>
2022-01-28 21:34:17 +0900

    2nd commit 実行例2に変更

--- Sort.c
+++ Sort.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void display_scores(int *values, const int SIZE) {
+void display_scores(int *values, const int SIZE) {   //* -> []の意味 const -> 定数(途中で値が変わらない)
     for (int i=0; i<SIZE; i++) {
         printf("%d ", values[i]);
     }
@@ -8,8 +8,8 @@ void display_scores(int *values, const int SIZE) {
 }
 
 int main(){
-    const int SIZE = 5;
-    int scores[SIZE] = {0, 60, 70, 100, 90};
+    const int SIZE = 6;
+    int scores[SIZE] = {100, 60, 70, 100, 90,80};
     int i, j, tmp;
 
     printf("scores = ");
